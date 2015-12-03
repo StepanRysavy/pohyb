103632561912e86a07e755a09514ecc3e6394aae
Stepan Rysavy <srysavy@vccp.com>
2015-12-03 11:15:28 +0000

    Removed local referneces in demo

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-(function (W, P, C, undefined) {
+(function (undefined) {
 
     var $ = function (query, element) {
 
@@ -11,11 +11,11 @@
     	}
     };
 
-    P.to($(".square"), 2, {left: 100});
+    Pohyb.to($(".square"), 1, {left: 100});
     /*
     P.to($(".square"), 2, {left: 0, delay: 3});
     P.to($(".circle"), 2, {top: 200, opacity: .2, delay: 1});
     P.to($(".circle"), 2, {top: 0, opacity: 1, delay: 4});
     /**/
     
-})(window, Pohyb, Cas);
+})();
